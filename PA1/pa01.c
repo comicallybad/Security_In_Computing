385e7519359849d55b8e4df375bccd5fdf726bf9
amp5646@yahoo.com <amp5646@yahoo.com>
2021-06-04 19:13:19 -0400

    seg fault possible fix

--- PA1/pa01.c
+++ PA1/pa01.c
@@ -108,19 +108,19 @@ int main(int *argsc, char **argsv)
         exit(1);
     }
 
-    char key[MAXLEN];
+    char *key = malloc(512);
     readVigenereKey(keyText, key);
     printf("\n\nVigenere Key:\n\n");
     printVigenere(key);
     printf("\n");
 
-    char plain[MAXLEN];
+    char *plain = malloc(512);
     readPlainText(plainText, plain);
     printf("\n\nPlaintext:\n");
     print80PerLine(plain);
     printf("\n");
 
-    char cipher[MAXLEN];
+    char *cipher = malloc(512);
     readCiphertext(plain, key, cipher);
     printf("\n\nCiphertext:\n");
     print80PerLine(cipher);
